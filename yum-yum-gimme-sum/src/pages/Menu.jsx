7e83d59f989721ea44c6371d3d0db5bfdfa01147
SimonGebru <te.gebru@hotmail.se>
2025-03-05 14:28:41 +0100

    Fortsatt småfix

--- yum-yum-gimme-sum/src/pages/Menu.jsx
+++ yum-yum-gimme-sum/src/pages/Menu.jsx
@@ -31,7 +31,7 @@ const Menu = () => {
     <div className="menu-page">
       <img src={logo2} alt="Yum Yum Gimme Sum Logo" className="logo2" />
       <div className="cart-container">
-        <img src={cartBox} alt="Cart Box" className="cart-box" />
+        <div alt="Cart Box" className="cart-box" />
         <img src={cartIcon} alt="Cart Icon" className="cart-icon" />
         <span className="cart-badge">6</span>
       </div>
@@ -39,17 +39,15 @@ const Menu = () => {
       <div className="menu-box">
         <h1>MENY</h1>
 
-        {/* Knappar för att byta kategori */}
-       
-
         {/* Övriga rätter */}
         <ul>
           {otherItems.length > 0 ? (
             otherItems.map((item) => (
-              <li key={item.id} className="menu-item">
+                <li key={item.id} className="menu-item">
                 <div className="menu-header">
                   <span className="menu-name">{item.name.toUpperCase()}</span>
-                  <span className="menu-price">................ {item.price} SEK</span>
+                  <span className="menu-line"></span>
+                  <span className="menu-price">{item.price} SEK</span>
                 </div>
                 {item.ingredients && (
                   <p className="menu-ingredients">{item.ingredients.join(", ")}</p>
@@ -64,7 +62,11 @@ const Menu = () => {
         {/* Dippsåser sist */}
         {dips.length > 0 && (
           <>
-            <h2 className="menu-dips-header">DIPSÅS ................................... 19 SEK</h2>
+            <div className="menu-dips-header">
+              <span>DIPSÅS</span>
+              <span className="menu-line"></span>
+              <span className="menu-price">19 SEK</span>
+            </div>
             <div className="dips-container">
               {dips.map((dip) => (
                 <span key={dip.id} className="dip-item">{dip.name}</span>
