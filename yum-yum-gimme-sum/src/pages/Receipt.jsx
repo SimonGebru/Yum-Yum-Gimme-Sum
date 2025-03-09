e8c952bc95b423f1304f14b81e830a64f3bd481f
SimonGebru <te.gebru@hotmail.se>
2025-03-09 23:02:25 +0100

    små fix med styling, nästa tillfälle behöver jag även hämta drycker från api och inte bara dip och wonton

--- yum-yum-gimme-sum/src/pages/Receipt.jsx
+++ yum-yum-gimme-sum/src/pages/Receipt.jsx
@@ -1,12 +1,12 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import logoCorner from "../assets/logo2.svg";
 import "../styles/receipt.scss";
 
 const Receipt = () => {
   const navigate = useNavigate();
 
-  // Hämtar orderinformation och cart från Redux-store
   const { orderNumber } = useSelector((state) => state.order);
   const { items, total } = useSelector((state) => state.cart);
 
@@ -16,27 +16,31 @@ const Receipt = () => {
 
   return (
     <div className="receipt-page">
-      <img src={logo} alt="YYGS Logo" className="receipt-logo" />
+      <img src={logoCorner} alt="Union Logo" className="union-logo" />
 
       <div className="receipt-container">
+        <img src={logo} alt="YYGS Logo" className="receipt-logo" />
         <h1>KVITTO</h1>
         <p className="receipt-order-number">#{orderNumber}</p>
 
         <ul className="receipt-items">
           {items.map((item) => (
             <li key={item.id} className="receipt-item">
-              <div className="receipt-item-header">
+              <div className="receipt-item-info">
                 <span className="receipt-item-name">{item.name.toUpperCase()}</span>
-                <span className="receipt-line"></span>
-                <span className="receipt-item-price">{item.price * item.quantity} SEK</span>
+                <span className="item-quantity">{item.quantity} stycken</span>
               </div>
-              <div className="receipt-item-quantity">{item.quantity} stycken</div>
+              <span className="receipt-line"></span>
+              <span className="receipt-item-price">{item.price * item.quantity} SEK</span>
             </li>
           ))}
         </ul>
 
         <div className="receipt-total-box">
-          <span className="total-text">TOTALT</span>
+          <div className="total-text">
+            TOTALT
+            <span className="moms">inkl 20% moms</span>
+          </div>
           <span className="total-price">{total} SEK</span>
         </div>
       </div>
